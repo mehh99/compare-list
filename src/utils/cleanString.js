9e6f532b60f778777f0e56bdbe95263cb1857848
mehh99 <iqbaltulilham23@gmail.com>
2024-09-22 03:35:28 +0700

    fix bugs

--- src/utils/cleanString.js
+++ src/utils/cleanString.js
@@ -1,8 +1,7 @@
 // src/utils/cleanString.js
 export function cleanString(str) {
-    // Hapus tanggal dan waktu dengan regular expression
-    // Contoh: "Jul 25, 2024, 10:37 PM" dihapus, menyisakan nama atau teks
-    const cleanedString = str.replace(/^[A-Za-z]+\s\d{1,2},\s\d{4},?\s\d{1,2}:\d{2}\s[APM]{2}|\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/g, '').trim();
-    return cleanedString;
-  }
-  
+  // Hapus tanggal dan waktu dengan regular expression
+  // Menghapus format seperti "4:20 pm", "11:00 pm", dll.
+  const cleanedString = str.replace(/(?:Sep|Oct|Nov|Dec|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug)\s\d{1,2},\s\d{4}\s\d{1,2}:\d{2}\s[APM]{2}|(?:Sep|Oct|Nov|Dec|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug)\s\d{1,2},\s\d{4}|\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}|(?:\d{1,2}:\d{2}\s[APM]{2}|\d{1,2}:\d{2})/g, '').trim();
+  return cleanedString;
+}
